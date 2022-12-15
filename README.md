9ba6441e82cf1cefa372b3cd0b31b1f53b1ef6d0
Thomas Chan <tchan@mtec.edu>
2022-12-15 15:46:59 -0700

    Did some cleanup before this commit

---
+++ README.md
@@ -0,0 +1,44 @@
+<h3 align="center">Express Intro</h3>
+
+## Getting Started
+
+This demo app used Express to build a simple RESTful APIs, the GET/POST request, with Node.js. To get a local copy up and running follow the installation steps below.
+
+### Prerequisites
+
+Make sure you already have Node.js installed. If not, [download](https://nodejs.org/en/download) and install Node.js.
+
+### Installation
+
+_Follow the following steps to install and run the demo app._
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/chan-thomas/express-intro.git
+   ```
+2. Install NPM packages
+   ```sh
+   npm install
+   ```
+3. Run the app
+   ```sh
+   node index.js
+   ```
+4. Point your browser to the following url
+   ```
+   http://localhost:4200/
+   ```
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+You can also use Postman to test out the APIs
+
+_For more examples, please refer to the Express [Documentation](https://expressjs.com/en/starter/installing.html)_
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const port = 4200
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+// localhost:port/
+app.get('/',(req, res)=>{
+    res.send(`<h2>Express demo: Good to have you</h2><div><a href="/adduser">Add a user</a></div>`)
+})
+// localhost:port/time
+app.get('/time', (req, res)=>{
+    const date = new Date()
+    console.log(date)
+    res.send(`<h2>Just a minute.</h2><div>The current time is: ${date}</div>`)
+})
+// localhost:port/file
+app.get('/file',(req, res)=>{
+    fs.readFile('./brownies.txt', 'utf-8', (err, data)=>{
+        res.send(`<pre>${data}</pre>`)
+    })
+})
+// localhost:port/adduser 
+app.get('/adduser',(req, res)=>{
+    fs.readFile('./index.html', 'utf-8', (err, data)=>{
+        res.send(`${data}`)
+    })
+})
+// html form or Postname
+app.post('/addUser',(req,res)=>{
+    const email = req.body.email
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+    console.log(`name: ${firstName} ${lastName}. email: ${email}`)
+    res.send(`Hi ${firstName} ${lastName}. Your email is: ${email}`)
+})
+
+// Postman with an arry of json objects
+// [{"firstName":"Lionel","lastName":"Messi","email":"forward@barcelona.edu"},
+//  {"firstName":"Cristiano","lastName":"Ronaldo","email":"football@realmadrid.edu"}]
+app.post('/addUsers',(req,res)=>{
+    const data = req.body
+    console.log(data)
+    console.log(JSON.stringify(data))
+    data.forEach(element => {
+        const email = element.email
+        const firstName = element.firstName
+        const lastName = element.lastName
+        console.log(`name: ${firstName} ${lastName}. email: ${email}`)
+    });
+    res.json(data)
+})
+
+app.listen(port, ()=>{
+console.log(`Server is up. Listening on port ${port}`);
+})
